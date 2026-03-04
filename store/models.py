4540ae7f5226bcffb98111ca0829a9ddc995ef5c
Sude Irmak <sudeirmak14@gmail.com>
2026-03-04 23:41:59 +0100

    Added complete database models for store and custom user

--- store/models.py
+++ store/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+STATUS_CHOICES = [
+    ("P", "Pending"),
+    ("A", "Approved"),
+    ("S", "Shipped"),
+]
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    tags = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart {self.id} - {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name} (Cart {self.cart.id})"
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    shipping_address = models.TextField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="P")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name} (Order {self.order.id})"
+
