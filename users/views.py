1aa16f8a05260fc089d136decc42994e3021be96
Sude Irmak <sudeirmak14@gmail.com>
2026-03-27 17:02:34 +0100

    Implemented auth flows (register, login, logout) with custom user model.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCretaionForm
 
-# Create your views here.
+class SignUpView(CreateView):
+    form_class = CustomUserCretaionForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
