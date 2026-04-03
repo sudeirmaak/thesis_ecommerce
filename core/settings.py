dcc5b54f4eab047acda49f02c4a0ed51bcae55c9
Sude Irmak <sudeirmak14@gmail.com>
2026-04-03 16:13:44 +0200

    fix base layout bugs, get static css/images working and make the frontend logic work

--- core/settings.py
+++ core/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
